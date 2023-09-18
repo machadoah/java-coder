88ec69c023969491a1b6168f71d4783044b81a73
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-09-18 15:18:02 -0300

    Inserindo dados a respeito de Spring Boot

---
+++ exercicios-sb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
@@ -0,0 +1,31 @@
+package com.example.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.exerciciossb.models.Cliente;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+	
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(18,"Pedro","123.456.789-00");
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Cliente obterClientePorId(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+	
+	// clientes?id=123/
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "João Augusto", "111.222.333-44");
+	}
+
+}
---
+++ exercicios-sb/src/main/java/com/example/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,20 @@
+package com.example.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	// @RequestMapping(method = RequestMethod.GET, path = "/ola")
+	@GetMapping(path = {"/ola","/saudacao"})
+	public String ola() {
+		return "Olá Spring Boot";
+	}
+	
+//	@GetMapping(path = "/saudacao")
+//	public String saudacao() {
+//		return "Olá Spring Boot";
+//	}
+
+}
